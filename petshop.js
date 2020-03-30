8305e25a6f89c965c5bcdfcd6869740036cc21af
Isa Mary <isa.kusuki@gmail.com>
2020-03-30 11:28:35 -0300

    for buscarPet

--- petshop.js
+++ petshop.js
@@ -1,3 +1,6 @@
+let divPet = document.getElementById('pets');
+
+
 let animal =  {
     tipo: "Cachorro",
     nome: "Dino",
@@ -40,7 +43,7 @@ let listaDeAnimais = [
     }
 }];
 
-function adicionarPet(tipo, nome, idade, raca, porte, sexo, vacinado, tutor){
+function adicionarPet(tipo, nome, idacondensadode, raca, porte, sexo, vacinado, tutor){
     let novoPet = {
             tipo:tipo,
             nome:nome,
@@ -61,15 +64,42 @@ function adicionarPet(tipo, nome, idade, raca, porte, sexo, vacinado, tutor){
 function visualizarPets(){
     for(let i = 0; i < listaDeAnimais.length; i++){
         let animal = listaDeAnimais[i];
-        console.log("------------------------------");
-        console.log("Nome do Animal: " + animal.nome);
-        console.log("Tipo de Animal: " + animal.tipo);
-        console.log("Sexo do Animal: " + animal.sexo);
-        console.log("Idade do Animal: " + animal.idade);
-        console.log("Raça do Animal: " + animal.raca);
-        console.log("Porte do Animal: " + animal.porte);
-        console.log("Animal Vacinado: " + animal.vacinado);
-        console.log("Nome do Tutor: " + animal.tutor.nome);
-        console.log("Telefone do Tutor: " + animal.tutor.telefone);
+
+        let divConteudoPet = document.createElement('div'); // <div></div>
+        divConteudoPet.setAttribute('class','bixinho'); // criando class
+
+        let pSeparador = document.createElement('p');
+        pSeparador.textContent = "------------------------------";
+        
+        let pNomeAnimal = document.createElement('p');
+        pNomeAnimal.textContent = "Nome do Animal: " + animal.nome;
+        
+        let pTipoAnimal = document.createElement('p');
+        pTipoAnimal.textContent = "Tipo de Animal: " + animal.tipo;
+        
+        divConteudoPet.appendChild(pSeparador);
+        divConteudoPet.appendChild(pNomeAnimal);
+        divConteudoPet.appendChild(pTipoAnimal);
+
+        divPet.appendChild(divConteudoPet);
+
+        //console.log("Sexo do Animal: " + animal.sexo);
+        //console.log("Idade do Animal: " + animal.idade);
+        //console.log("Raça do Animal: " + animal.raca);
+        //console.log("Porte do Animal: " + animal.porte);
+        //console.log("Animal Vacinado: " + animal.vacinado);
+        //console.log("Nome do Tutor: " + animal.tutor.nome);
+        //console.log("Telefone do Tutor: " + animal.tutor.telefone);
     }
+}
+
+function buscarPet(nomePet) {
+    for(i=0; i <listaDeAnimais.length; i++) {
+        let animal = listaDeAnimais[i];
+
+        if(animal.nome == nomePet){
+            return console.log("Sim, existe esse animal cadastrado no sistema.")
+        }
+    }
+    console.log("Não existe animal cadastrado.")
 }
